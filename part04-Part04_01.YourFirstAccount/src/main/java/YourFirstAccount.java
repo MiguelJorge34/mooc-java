29aa06fa35974b39cd87a8c9f2a01030e89cbfaa
Miguel Alexandre Pereira Jorge <migueljorge@Air-de-Miguel.lan>
2023-04-17 21:10:35 +0100

    exe

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,10 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        Account testBobAccount = new Account("Miguel", 100.0);
+        
+        testBobAccount.deposit(20.0);
+        
+        System.out.println(testBobAccount);
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -2,7 +2,16 @@
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
+        Account testAccount1 = new Account("Matthews account", 1000.0);
+        
+        Account testAccount = new Account("My account", 0.0);
+        
+        testAccount1.withdrawal(100.0);
+        
+        testAccount.deposit(100.0);
+        
+        System.out.println(testAccount1.toString());
+        System.out.println(testAccount.toString());
+        
     }
 }
