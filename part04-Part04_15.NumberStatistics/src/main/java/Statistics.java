6b71b9e11bd79951040c15119ad107e52b29e39a
Miguel Alexandre Pereira Jorge <migueljorge@Air-de-Miguel.lan>
2023-04-17 23:27:14 +0100

    exe

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,35 @@
 
+import java.util.ArrayList;
+
+
 public class Statistics {
-}
+    
+    private int count;
+    private int sum;
+    
+    public Statistics(){}
+    
+    public void addNumber(int number){
+        this.sum = sum + number;
+        this.count++;
+        
+    }
+    
+    public int getCount(){
+        return count;
+    }
+    
+    public int sum (){
+        
+        
+        return this.sum;
+    }
+    public double average() {
+        double result = 0;
+
+        if (this.sum != 0 && this.count != 0) {
+            result = 1.0 * this.sum / this.count;
+        }
+        return result;
+    }
+ }
