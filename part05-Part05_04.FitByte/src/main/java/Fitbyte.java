e9a91d92585e31604c861ef87e8756e9db08bb9d
Miguel Alexandre Pereira Jorge <migueljorge@Air-de-Miguel.lan>
2023-04-18 23:24:50 +0100

    exe

---
+++ part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author migueljorge
+ */
+public class Fitbyte {
+    
+    private int age;
+    private int restingHeartRate;
+    
+    public Fitbyte(int age, int restingHeartRate){
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+    }
+    
+     public double targetHeartRate(double percentageOfMaximum) {
+        
+        double result = 0;
+        double maxHeartRate = 0;
+        maxHeartRate = 206.3 - (0.711 * age);
+        
+        result = (maxHeartRate - restingHeartRate) * (percentageOfMaximum) + restingHeartRate;
+        
+        
+        
+        return result;
+    }
+    
+}
