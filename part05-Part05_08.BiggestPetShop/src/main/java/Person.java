9138a48cd13550cc8c945ebaa57b3427ea99a1b6
Miguel Alexandre Pereira Jorge <migueljorge@MacBook-Air-de-Miguel.local>
2023-04-19 19:57:36 +0100

    exe

--- part05-Part05_08.BiggestPetShop/src/main/java/Person.java
+++ part05-Part05_08.BiggestPetShop/src/main/java/Person.java
@@ -19,7 +19,7 @@ public class Person {
 
     @Override
     public String toString() {
-        return this.name;
+        return name + " has a friend called " + pet.getName() +" ("+pet.getBreed()+")";
     }
 
 }
--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,47 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    
+    public int getSize(){
+        return squares;
+    }
+    public int getPrices(){
+        return squares * princePerSquare;
+    }
+    
+    public boolean largerThan(Apartment compared){
+        if(this.squares > compared.getSize()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public int priceDifference(Apartment compared){
+        
+        int difference = compared.getPrices() - this.getPrices();
+        
+        return Math.abs(difference);
+        
+    
+    }
+    public int priceDifference1(Apartment compared){
+        
+        int difference = compared.getPrices() - this.getPrices();
+        
+        return (difference);
+        
+    
+    }
+    
+     public boolean moreExpensiveThan(Apartment compared) {
+         if(priceDifference1(compared) > 0 ){
+             return false;
+         }
+         else {
+             return true;
+         }
+         
+     }
 
 }
